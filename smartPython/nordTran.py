f410d3aabadc9535e6caf806e61a10df45fdafde
aisikawaa <alterthemadness@outlook.com>
2020-09-03 12:32:39 +0900

    Add nirdTran.py nordTran.py is ast module exp

---
+++ smartPython/nordTran.py
@@ -0,0 +1,20 @@
+import ast
+
+class ReplaceBinOp(ast.NodeTransformer):
+    """二項演算を加算に置き換える"""
+    def visit_BinOp(self, node):
+        return ast.BinOp(left=node.left,
+                         op=ast.Add(),
+                         right=node.light)
+
+tree = ast.parse("x = 1/3")
+ast.fix_missing_locations(tree)
+eval(compile(tree, '', 'exec'))
+print(ast.dump(tree))
+print(x)
+
+tree = ReplaceBinOp().visit(tree)
+ast.fix_missing_locations(tree)
+print(ast.dump(tree))
+eval(compile(tree, '', 'exec'))
+print(x)
