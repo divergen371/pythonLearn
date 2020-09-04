f440693c0059b6f617343ed5515541b7da9a0bb1
aisikawaa <alterthemadness@outlook.com>
2020-09-04 22:53:14 +0900

    update

--- smartPython/nordTran.py
+++ smartPython/nordTran.py
@@ -1,20 +1,21 @@
 import ast
 
+
 class ReplaceBinOp(ast.NodeTransformer):
     """二項演算を加算に置き換える"""
+
     def visit_BinOp(self, node):
-        return ast.BinOp(left=node.left,
-                         op=ast.Add(),
-                         right=node.right)
+        return ast.BinOp(left=node.left, op=ast.Add(), right=node.right)
+
 
 tree = ast.parse("x = 1/3")
 ast.fix_missing_locations(tree)
-eval(compile(tree, '', 'exec'))
+eval(compile(tree, "", "exec"))
 print(ast.dump(tree))
 print(x)
 
 tree = ReplaceBinOp().visit(tree)
 ast.fix_missing_locations(tree)
 print(ast.dump(tree))
-eval(compile(tree, '', 'exec'))
+eval(compile(tree, "", "exec"))
 print(x)
