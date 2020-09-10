c1566ae777c08c0d4dbe0c4de0742cb40c0cb8d0
aisikawaa <alterthemadness@outlook.com>
2020-09-10 19:48:53 +0900

    update

--- smartPython/append.py
+++ smartPython/append.py
@@ -1,4 +1,6 @@
-numbers = [0, -6, 1, -8, 2, 3, 4, 5, -1, -2, -3, -4, -5, -8, 6, 7, 8, -9]
+from typing import Any, List, Union
+
+numbers: List[Union[int, Any]] = [0, -6, 1, -8, 2, 3, 4, 5, -1, -2, -3, -4, -5, -8, 6, 7, 8, -9]
 positive = []
 
 for i in range(len(numbers)):
